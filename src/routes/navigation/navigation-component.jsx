127b0b2b77aa244b2280d0015346595928b1f01c
github_username <email_address>
2024-04-19 12:28:11 +0300

    created sign up component, the form input component, button component

--- src/routes/navigation/navigation-component.jsx
+++ src/routes/navigation/navigation-component.jsx
@@ -1,28 +1,28 @@
+import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { ReactComponent as NeXLogo } from '../../assets/nex.svg';
 import { Fragment } from 'react';
-import { ReactComponent as NeXLogo} from '../../assets/nex.svg'
 import './navigation.styles.scss';
-
 
 const Navigation = () => {
     return (
-    <Fragment>
-    <div className='navigation'>
-    <Link className='logo-container' to='/' >
-        <NeXLogo className='logo'/>
-        </Link>
-        <div className='nav-links-container'>
-        <Link className='nav-link' to='/shop' >
-            SHOP
-        </Link>
-        <Link className='nav-link' to='/sign-in' >
-        SIGN IN
-    </Link>
-        </div>
-    </div>
-    <Outlet />
-    </Fragment>
-);
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'>
+                    <NeXLogo className='logo' />
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
+                    </Link>
+                    <Link className='nav-link' to='/sign-in'>
+                        SIGN IN
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    );
 };
 
 export default Navigation;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
-import {
-    getAuth,
-    signInWithPopup,
-    GoogleAuthProvider
+import { 
+    getAuth, 
+    signInWithPopup, 
+    signInWithRedirect, 
+    GoogleAuthProvider, 
+    createUserWithEmailAndPassword 
 } from 'firebase/auth';
-import {
-    getFirestore,
-    doc,
-    getDoc,
-    setDoc
+import { 
+    getFirestore, 
+    doc, 
+    getDoc, 
+    setDoc 
 } from 'firebase/firestore';
 
 const firebaseConfig = { 
@@ -20,37 +22,42 @@ const firebaseConfig = {
     appId: "1:815334568675:web:4462acad992b135e8ccbd6" 
 };
 
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
-
 provider.setCustomParameters({
     prompt: "select_account"
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
-    const userDocRef = doc(db, 'users', userAuth.uid);
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+    if (!userAuth) return;
 
+    const userDocRef = doc(db, 'users', userAuth.uid);
     const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot);
-    console.log(userSnapshot.exists());
 
     if (!userSnapshot.exists()) {
-        const { displayName,  email } = userAuth;
+        const { displayName, email } = userAuth;
         const createdAt = new Date();
-
         try { 
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation
             });
         } catch(error) {
             console.log('error creating the user', error.message);
         }
     }
     return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);
 };

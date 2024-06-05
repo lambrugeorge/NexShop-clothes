2c99221d5f5070b078b0d9802a6e3dcefd78cbc8
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-06-05 22:15:50 +0300

    PWA in application

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { Elements } from "@stripe/react-stripe-js";
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from "./App";
 import { store, persistor } from "./store/store";
 import { stripePromise } from "./utils/stripe/stripe.utils";
@@ -27,3 +27,6 @@ render(
   </React.StrictMode>,
   rootElement
 );
+
+
+serviceWorkerRegistration.register();

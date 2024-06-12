04b65989e971f7625a4c295a5380990f58df8655
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-06-12 22:50:41 +0300

    added react testing library on Button Tests

---
+++ src/components/button/__tests__/button.component.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Button from '../button.component';
+
+describe('button tests', () => {
+       test('should render base button when nothing is passed', () => {
+              render(<Button/>);
+              const buttonElement = screen.getByRole('button');
+              expect(buttonElement).toHaveStyle('background-color: white');
+       });
+});

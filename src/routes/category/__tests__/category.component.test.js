bd2e1eca311872e4e74b54e3b56cda1f75326b85
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-06-14 19:22:59 +0300

    Refactor Category component tests and mock useParams for better isolation

---
+++ src/routes/category/__tests__/category.component.test.js
@@ -0,0 +1,52 @@
+import { screen } from "@testing-library/react";
+import Category from "../category.component";
+import { renderWithProviders } from "../../../utils/test/test.utils";
+import { useParams } from 'react-router-dom';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({
+    category: 'mens',
+  }),
+}));
+
+describe('Category tests', () => {
+  test('It should render a Spinner if isLoading is true', () => {
+    renderWithProviders(<Category />, {
+      preloadedState: {
+        categories: {
+          isLoading: true,
+          categories: [],
+        },
+      },
+    });
+
+    const spinnerElement = screen.getByTestId('spinner');
+    expect(spinnerElement).toBeInTheDocument();
+  });
+
+  test('It should render Categories if isLoading is false', () => {
+    renderWithProviders(<Category />, {
+      preloadedState: {
+        categories: {
+          isLoading: false,
+          categories: [
+            {
+              title: 'mens',
+              items: [
+                { id: 1, name: 'Product 1' },
+                { id: 2, name: 'Product 2' },
+              ],
+            },
+          ],
+        },
+      },
+    });
+
+    const spinnerElement = screen.queryByTestId('spinner');
+    expect(spinnerElement).toBeNull();
+
+    const product1Element = screen.getByText(/product 1/i);
+    expect(product1Element).toBeInTheDocument();
+  });
+});
